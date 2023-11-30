7735d3b5e7249e75a3e41e73cf0983651585f47d
HamishWHC <20359560+HamishWHC@users.noreply.github.com>
2023-12-01 10:53:12 +1100

    Load sfs commands conditionally.

--- scripts/sfs.sh
+++ scripts/sfs.sh
@@ -1,22 +1,24 @@
-sfs() {
-    if [ "$#" -ne 1 ]; then
-        echo "Usage: sfs <target>"
-        return 1
-    fi
+if command -v sshfs >/dev/null; then
+    sfs() {
+        if [ "$#" -ne 1 ]; then
+            echo "Usage: sfs <target>"
+            return 1
+        fi
 
-    target="$1"
+        target="$1"
 
-    mkdir -p $HOME/mounts/${target}
-    umount -f $HOME/mounts/${target}
-    sshfs -o idmap=user -C ${target}: $HOME/mounts/${target}
-}
+        mkdir -p $HOME/mounts/${target}
+        umount -f $HOME/mounts/${target}
+        sshfs -o idmap=user -C ${target}: $HOME/mounts/${target}
+    }
 
-usfs() {
-    if [ "$#" -ne 1 ]; then
-        echo "Usage: usfs <target>"
-        return 1
-    fi
+    usfs() {
+        if [ "$#" -ne 1 ]; then
+            echo "Usage: usfs <target>"
+            return 1
+        fi
 
-    target="$1"
-    umount -f $HOME/mounts/${target}
-}
+        target="$1"
+        umount -f $HOME/mounts/${target}
+    }
+fi
