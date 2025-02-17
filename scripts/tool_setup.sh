3c30956d0fca44310b6f5ef4b468070e9fc182ee
HamishWHC <20359560+HamishWHC@users.noreply.github.com>
2025-02-17 11:57:21 +1100

    fix tool setup

--- scripts/tool_setup.sh
+++ scripts/tool_setup.sh
@@ -3,9 +3,9 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 
 # zsh autocompletion
-autoload bashcompinit && bashcompinit
-autoload -U compinit
-compinit
+# autoload bashcompinit && bashcompinit
+# autoload -U compinit
+# compinit
 
 # fzf-tab for nice autocompletion interface
 source $HOME/.dotfiles/plugins/fzf-tab/fzf-tab.plugin.zsh
