0eb8ee414308c5ab9cf61e4b1b24263b54cceaef
HamishWHC <20359560+HamishWHC@users.noreply.github.com>
2026-06-17 20:57:39 +1000

    force mmf file

--- modules/home/darwin/default.nix
+++ modules/home/darwin/default.nix
@@ -1,6 +1,8 @@
 {
   flake.modules.homeManager.darwin = {
-    home.file."Library/Application Support/com.nuebling.mac-mouse-fix/config.plist".source =
-      ./mac-mouse-fix.plist;
+    home.file."Library/Application Support/com.nuebling.mac-mouse-fix/config.plist" = {
+      source = ./mac-mouse-fix.plist;
+      force = true;
+    };
   };
 }
