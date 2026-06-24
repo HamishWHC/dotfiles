41298a78719fdf981b6956b17b18d8c86584c766
HamishWHC <20359560+HamishWHC@users.noreply.github.com>
2026-06-24 12:42:24 +1000

    fix pkgs

--- modules/home/workstation/default.nix
+++ modules/home/workstation/default.nix
@@ -1,15 +1,17 @@
 { self, ... }:
 {
-  flake.modules.homeManager.workstation = {
-    imports = [
-      self.modules.homeManager.runtimes
-      self.modules.homeManager.ghostty
-      self.modules.homeManager.vscode
-      self.modules.homeManager.nix-tools
-    ];
+  flake.modules.homeManager.workstation =
+    { pkgs, ... }:
+    {
+      imports = [
+        self.modules.homeManager.runtimes
+        self.modules.homeManager.ghostty
+        self.modules.homeManager.vscode
+        self.modules.homeManager.nix-tools
+      ];
 
-    home.packages = with self.pkgs; [
-      ghidra
-    ];
-  };
+      home.packages = with pkgs; [
+        ghidra
+      ];
+    };
 }
