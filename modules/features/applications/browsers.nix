39bcff45fea33c3760dddf9ac65dd1b5c48c1d12
HamishWHC <20359560+HamishWHC@users.noreply.github.com>
2026-09-11 13:38:36 +1000

    Disable Firefox auto-updates

--- modules/features/applications/browsers.nix
+++ modules/features/applications/browsers.nix
@@ -14,7 +14,10 @@
         home.packages =
           with pkgs;
           [
-            unstable.firefox-bin
+            (unstable.firefox-bin.override {
+              # Bundle policies.json with Firefox so Nix manages browser updates.
+              extraPolicies.DisableAppUpdate = true;
+            })
           ]
           ++ lib.optional (pkgs.stdenv.isLinux) pkgs.unstable.ungoogled-chromium;
       };
